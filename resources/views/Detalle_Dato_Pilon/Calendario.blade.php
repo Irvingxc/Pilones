7a4a1a45166c174dd6af82ec48c70394daf02577
Irvingxc <48892124+Irvingxc@users.noreply.github.com>
2021-02-16 09:47:11 -0600

    Aporte
    Actualizacion en calendario

--- resources/views/Detalle_Dato_Pilon/Calendario.blade.php
+++ resources/views/Detalle_Dato_Pilon/Calendario.blade.php
@@ -8,6 +8,32 @@
 </div>
 
 </div>
+
+<div class="modal fade" id="agenda_modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Temperatura Pilon</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div>
+      <label for="" class="col-md-5">Fecha Actual</label>
+      <input type="text" class="col-md-6">
+      </div>
+        <label for="" class="col-md-5">Temperatura del Pilon</label>
+        <input type="text" class="col-md-6">
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Guardar Cambios</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
 
 <link href='{{asset("/packages/core/main.css")}}' rel='stylesheet' />
@@ -21,8 +47,9 @@
 <script src="{{asset('packages/interaction/main.js')}}"></script>
 <script src="{{asset('packages/daygrid/main.js')}}"></script>
 <script src="{{asset('packages/timegrid/main.js')}}"></script>
-<script src="{{asset('packages/bootstrap/main.js')}}"></script>
+
 <script src="{{asset('packages/core/locale/es.js')}}"></script>
+ 
 
 <script>
 document.addEventListener('DOMContentLoaded', function() {
@@ -36,6 +63,14 @@ document.addEventListener('DOMContentLoaded', function() {
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
+      
+      selectable: true,
+      selectMirror: true,
+      select: function(arg) {
+        $("#agenda_modal").modal();
+        calendar.unselect()
+      },
+      editable: true,
     });
     calendar.render();
 });
