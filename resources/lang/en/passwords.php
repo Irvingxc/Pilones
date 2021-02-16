e0949f1a2d5147e388be8876a92fd07691a352b3
Edgardo Sanabria <sanabria.edgardo24@gmail.com>
2021-02-16 14:13:08 -0600

    Update passwords.php

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => '¡Tu contraseña a sido cambiada!',
+    'sent' => '¡Te hemos enviado tu codigo para cambiar la contraseña!',
+    'throttled' => 'Por favor espera antes de reintentar.',
+    'token' => 'Este codigo de cambio de contraseña es invalido.',
+    'user' => "No encontramos un usuario con esa direccion de correo electronico.",
 
 ];
