6165db34b5c7014c0bfea2cfb89b851d05da97d3
Edgardo Sanabria <sanabria.edgardo24@gmail.com>
2021-02-25 07:16:42 -0600

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,14 +8,19 @@
                 <div class="card-header">{{ __('Registrarse') }}</div>
 
                 <div class="card-body"> 
-                    <form method="POST" action="{{ route('register') }}">
+                    <form @isset($register) method="post" action="{{route('verusuario.update', ['users'=>$register->id])}}" 
+                    @else 
+                     method="POST" action="{{ route('verusuario.store') }}"
+                     @endisset>
                         @csrf
 
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input  @isset($register)
+                                    value="{{$register->name}}" 
+                                    @endisset id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +34,9 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Direccion de correo electronico') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input  @isset($register)
+                                value="{{$register->email}}"
+                                @endisset id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -43,7 +50,9 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input  @isset($register)
+                                value="{{$register->password}}" 
+                                @endisset id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
