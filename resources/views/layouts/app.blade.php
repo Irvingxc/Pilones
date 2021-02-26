bf850455e4b341ef83a603c4d30e78f659a2e2e1
belen2298 <72229844+belen2298@users.noreply.github.com>
2021-02-26 11:38:00 -0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,7 +95,7 @@ h1 {
     <title>{{ config('app.name', 'Laravel') }}</title>
  
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" ></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -176,7 +176,7 @@ h1 {
             @yield('content')
         </main>
     </div>
-    <script src="{{asset('/js/app.js')}}"></script>
+  
 
     @yield('js')
 
