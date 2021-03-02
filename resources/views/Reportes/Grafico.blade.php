5063419f536fc666946ce748c2b2064b08a64bd0
belen2298 <72229844+belen2298@users.noreply.github.com>
2021-03-02 07:17:16 -0600

    Update Grafico.blade.php

--- resources/views/Reportes/Grafico.blade.php
+++ resources/views/Reportes/Grafico.blade.php
@@ -1,3 +1,43 @@
-<html>
+@extends('layouts.app')
+@section('content')
 
-</html>
+<div id="container" style="width: 100%;height: 80vh">
+	  <canvas id="myChart"></canvas>
+	  
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+
+<script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+        datasets: [{
+            label: 'Marzo',
+            backgroundColor: 'rgb(255,255,255,0.1)',
+            borderColor: 'rgb(0, 0, 128)',
+            data: [0, 60, 5, 90, 20, 30, 45,21,15,22,28,25]
+        }]  
+    },
+
+    // Configuration options go here
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend:{
+            position:'top'
+        },
+        title:{
+                dislpay:true,
+                text:'Chart.js Line chart'
+        }
+    }
+});
+</script>
+@endsection
+
